9c7722968e8837468ff06ae104ad8cb1e444a3d8
XiongZhun <igis_xc@fiberhome.com>
2016-03-31 15:12:27 +0800

    pac

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 'use strict';
 var AV = require('leanengine');
 
-var APP_ID = process.env.LC_APP_ID;
-var APP_KEY = process.env.LC_APP_KEY;
-var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
+var APP_ID = J79qPAf1e1Ilqb0l8Uf1NJKj-MdYXbMMI;
+var APP_KEY = 0n2AUcsdNUqzqNo7Vp8CfgV4;
+var MASTER_KEY = 	WQNKH2fUhoH3Y0WLmbk9zGRn;
+
+//var APP_ID = process.env.LC_APP_ID;
+//var APP_KEY = process.env.LC_APP_KEY;
+//var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
 
 AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
